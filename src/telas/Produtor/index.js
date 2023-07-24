f23ea6c7bf8b81e10fbec60941857f4d909d4eae
Otavio Henrique de Lima e Silva <otaviohlsilva@gmail.com>
2023-07-24 19:06:41 -0300

    commit 7

--- src/telas/Produtor/index.js
+++ src/telas/Produtor/index.js
@@ -1,14 +1,34 @@
 import React from 'react';
-import { FlatList, StyleSheet } from 'react-native';
-import { useRoute, } from '@react-navigation/native';
+import { FlatList, StyleSheet, View, Image, Text } from 'react-native';
+import { useRoute } from '@react-navigation/native';
 import Cesta from './componentes/Cesta';
+import Topo from "../../components/Topo";
+import useTextos from '../../hooks/useTextos';
+import topo from '../../assets/produtores/topo.png';
 
 export default function Produtor() {
   const route = useRoute();
+  const { tituloProdutor, tituloCestas } = useTextos();
   const { nome, imagem, cestas } = route.params;
+
+  const TopoLista = () => {
+    return (
+      <>
+        <Topo titulo={tituloProdutor} imagem={topo} altura={150} />
+        <View style={estilos.conteudo}>
+          <View style={estilos.logo}>
+            <Image source={imagem} style={estilos.produtorImage} />
+            <Text style={estilos.produtor}>{nome}</Text>
+          </View>
+          <Text style={estilos.cestas}>{tituloCestas}</Text>
+        </View>
+      </>
+    );
+  };
 
   return (
     <FlatList
+      ListHeaderComponent={TopoLista}
       data={cestas}
       renderItem={({ item }) => (
         <Cesta {...item} produtor={{ nome, imagem }} />
